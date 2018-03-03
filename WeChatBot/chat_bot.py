c9c573e24e555fee25e33e684aabd734c02d9237
Maye <huchi97@foxmail.com>
2018-03-03 08:40:03 +0800

    Add debug info for more convenience

--- WeChatBot/chat_bot.py
+++ WeChatBot/chat_bot.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import json
+
 from requests_html import HTMLSession
 from wxpy import *
 import time
@@ -24,13 +26,20 @@ bot = Bot()
 # 别太贪心，一天三个红包还嫌不够吗
 while count<=3:
 
-    # 定时任务：每过5分钟读取一次新的消息并尝试抢红包
-    time.sleep(300)
+    # 定时任务：每过3秒读取一次新的消息并尝试抢红包
+    time.sleep(3)
 
     for i,sharing in enumerate(bot.messages.search('饿了么')+bot.messages.search('美团')):
         r = session.post(server, {'url': sharing.url, 'mobile': mobile, 'headers': header})
-        if mobile in r.text:
+        if r.text:
             print('已抢了{}次红包~'.format(count+1))
-            count+=1
+            msg = json.loads(r.text)
+            try:
+                print(msg['message'])
+            except:
+                pass
+            print('\n-----------------\n')
+            if '领取完毕' in msg['message']:
+                count+=1
         if bot.messages:
             bot.messages.pop(i)
